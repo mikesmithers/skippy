349fa95b3e29dc0ad47004593f44bcf26f97a24d
mike <antikyte@gmail.com>
2026-04-03 12:18:36 +0100

    SKIPPY_LOGS table creation now includes EXTRA column and length semantic changes. An Upgrade script added to apply these changes as well as the new package version to existing SKIPPY installations.

---
+++ upgrade1.sql
@@ -0,0 +1,20 @@
+--
+-- Script to upgrade an existing installation.
+-- Changes applied by this script are :
+-- 1) Add the EXTRA column to SKIPPY_LOGS
+-- 2) Convert SKIPPY_LOGS VARCHAR columns to use CHAR length semantics explicitly
+-- 3) Add the facility to log parameters using  SKIPPY.T_PARAMS_TYPE 
+--
+
+prompt Adding column to SKIPPY_LOGS...
+prompt
+@tables/skippy_logs_add_extra.sql
+
+prompt Converting SKIPPY_LOGS to use CHAR length semantics...
+prompt
+@tables/skippy_logs_char_semantics.sql
+
+prompt Updating package SKIPPY...
+
+@packages/skippy.pks
+@packages/skippy.pkb
